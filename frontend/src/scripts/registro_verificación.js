f11144f461129e4c07e06dd326ee365a19f70e30
JPiñaSuarez <juanpsuarez.pia@gmail.com>
2024-05-19 22:35:19 -0600

    Mas correcciones

--- frontend/src/scripts/registro_verificación.js
+++ frontend/src/scripts/registro_verificación.js
@@ -8,12 +8,14 @@ const valid = document.querySelector(".verify-control");
 const codigoInput = document.getElementById("codigo-input");
 const buttonVerify = document.getElementById("b-verify");
 
+// Desactivar el botón de verificación
+buttonVerify.disabled = true;
+
+
 let codigoGenerado; // Variable para almacenar el código generado
 
 // Función para mostrar la alerta y solicitar el código
 function mostrarAlerta() {
-    // Generar un nuevo código
-    codigoGenerado = generarCodigo();
 
     // Mostrar una alerta con el código generado
     alert("Tu código de verificación es: " + codigoGenerado);
@@ -25,24 +27,28 @@ function mostrarAlerta() {
     codigoInput.style.display = "block";
 }
 
-// Desactivar el botón al mostrar el nuevo código
-buttonVerify.disabled = true;
-
 // Función para validar el código ingresado
 function validarCodigoIngresado() {
     const codigoIngresado = parseInt(codigoInput.value, 10);
-    return codigoGenerado === codigoIngresado;
+    console.log("Código generado:", codigoGenerado);
+    console.log("Código ingresado:", codigoIngresado);
+    if (codigoGenerado === codigoIngresado) {
+        // Si el código ingresado es correcto, activar el botón de verificación
+        buttonVerify.disabled = false;
+    } else {
+        // Si el código ingresado no es correcto, mantener el botón desactivado
+        buttonVerify.disabled = true;
+    }
 }
 
 // Agregar un evento de clic al elemento de validación
-valid.addEventListener("click", mostrarAlerta);
+valid.addEventListener("click", function() {
+    // Generar un nuevo código de verificación
+    codigoGenerado = generarCodigo();
+    // Mostrar la alerta
+    mostrarAlerta();
+});
 
 // Agregar un evento de entrada al campo de entrada para validar el código ingresado y habilitar el botón
-codigoInput.addEventListener("input", () => {
-    if (validarCodigoIngresado()) {
-        buttonVerify.disabled = false;
-    } else {
-        buttonVerify.disabled = true;
-    }
-});
+codigoInput.addEventListener("input", validarCodigoIngresado);
 
