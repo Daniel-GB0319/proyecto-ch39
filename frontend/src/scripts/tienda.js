b4fd56e39b4f9896f54a7443beef9f4bd399c61f
Mauricio <gnoe8799@gmail.com>
2024-05-30 13:15:56 -0600

    script de tienda para url de api

--- frontend/src/scripts/tienda.js
+++ frontend/src/scripts/tienda.js
@@ -15,9 +15,35 @@ function handleCompra(event) {
 
   console.log(`Se ha comprado ${nombreProducto}`);
 }
+document.addEventListener('click', function() {
+  window.location.href = 'producto.html';
+});
+
 
 const botonesCompra = document.querySelectorAll('.btn-carro');
 
 botonesCompra.forEach(boton => {
   boton.addEventListener('click', handleCompra);
 });
+
+
+const inormacion =document.getElementById(`contenedor`);
+ informacion. addEventListener(`click`, () =>{
+  const nomdelproducto=document.getElementById(`producto`).ariaValueMax;
+  
+  
+  fetch(URL)
+  .then (response => response.json())
+  .then(Data => {
+    userinfo.innerHTML=`
+    <spam>nombrel del producto: ${Data.producto }</spam>
+    <spam> el precio del producto es  ${data.precio} </spam>
+
+
+    `
+  })
+  .catch(error => {
+    console.error(error)
+  })
+  
+ })
